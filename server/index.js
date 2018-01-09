70192e03edd466736ee19a456ff20ac7a2161a0a
trc5019 <trc5019@gmail.com>
2018-01-09 15:48:56 +0000

    got oauth to work

--- server/index.js
+++ server/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const passport = require("passport");
+const GithubStrategy = require("passport-github2").Strategy;
+const { githubClientID, githubClientSecret } = require("./config/keys");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+passport.use(
+    new GithubStrategy(
+        {
+            clientID: githubClientID,
+            clientSecret: githubClientSecret,
+            callbackURL: "/auth/github/callback"
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log(profile);
+        }
+    )
+);
+
+app.get(
+    "/auth/github",
+    passport.authenticate("github", {
+        scope: ["read:user", "read:org"]
+    })
+);
+
+app.get("/auth/github/callback", passport.authenticate("github"));
 app.disable("x-powered-by");
 // app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
