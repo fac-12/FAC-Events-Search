91b120f7c9a071b44509bb7a85c2eeac9b101982
trc5019 <trc5019@gmail.com>
2018-01-09 13:59:41 +0000

    set up proxy on client

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    githubClientID: process.env.githubClientID,
+    githubClientSecret: process.env.githubClientSecret,
+    DATABASE_URL: process.env.DATABASE_URL,
+    COOKIE_KEY: process.env.COOKIE_KEY
+};
