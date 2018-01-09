c14627a721cd655c871e4697d43b877f0aaf9ce6
trc5019 <trc5019@gmail.com>
2018-01-09 17:25:55 +0000

    setting up react-front-end

---
+++ server/client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+            <Switch>
+                <Route path="/" component={LandingPage} />
+      </Switch>
+      </BrowserRouter>
+  </Provider>
+);
