bdd300e4ab790e6d0c54cd58b6ad39abbfc1869f
trc5019 <trc5019@gmail.com>
2018-01-10 22:44:39 +0000

    created  footer and banner components

--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "../style.css";
 
 export default () => (
   <div>
-    <h1> Banner (to be built) </h1>
+    <h1 className="banner-title">
+      Find events of interest to the FAC london community{" "}
+    </h1>
+    <p className="banner-tagline">
+      See who else is going, chat about plans, and review past events
+    </p>
   </div>
 );
