5bf11d1826e260b6453f581e2e6ade949e1c4b13
Shannon Jensen <shannonjensen@gmail.com>
2018-01-10 10:59:51 +0000

    Refactor App.js, rename files to match convention, set up auth reducer structure

--- client/src/components/landingPage.js
+++ client/src/components/landingPage.js
@@ -1,14 +1,10 @@
 import React from "react";
-import NavBar from "./navBar";
-import Banner from "./banner";
-import Footer from "./footer";
+import Banner from "./Banner";
 import EventsPreview from "../containers/EventsPreviews";
 
 export default () => (
   <div>
-    <NavBar />
     <Banner />
     <EventsPreview />
-    <Footer />
   </div>
 );
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
-  state: {}
+  auth: authReducer
 });
 
 export default rootReducer;
