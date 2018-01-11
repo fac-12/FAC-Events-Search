db62eff4f5cf05c90edf82f71a97c03add86e8b8
Shannon Jensen <shannonjensen@gmail.com>
2018-01-11 20:06:07 +0000

    add Interest routes and actions

--- database/queries.js
+++ database/queries.js
@@ -24,24 +24,44 @@ const checkEvent = data =>
     .then(res => res.length > 0)
     .catch(e => console.log("db error", e));
 
-const addEvent = data => db
-  .query(
-    `INSERT INTO events(event_name, event_date, event_time, host_org_name, venue_name, venue_address, venue_postcode, event_url, event_desc) VALUES($1,$2,$3,$4,$5,$6,$7,$8, $9) RETURNING event_name`,
-    [
-      data.event_name,
-      data.event_date,
-      data.event_time,
-      data.host_org_name,
-      data.venue_name,
-      data.venue_address,
-      data.venue_postcode,
-      data.event_url,
-      data.event_desc
-    ]
-  )
-  .then(res => res[0].event_name)
-  .catch(e => console.log("db error", e));
+const addEvent = data =>
+  db
+    .query(
+      `INSERT INTO events(event_name, event_date, event_time, host_org_name, venue_name, venue_address, venue_postcode, event_url, event_desc) VALUES($1,$2,$3,$4,$5,$6,$7,$8, $9) RETURNING event_name`,
+      [
+        data.event_name,
+        data.event_date,
+        data.event_time,
+        data.host_org_name,
+        data.venue_name,
+        data.venue_address,
+        data.venue_postcode,
+        data.event_url,
+        data.event_desc
+      ]
+    )
+    .then(res => res[0].event_name)
+    .catch(e => console.log("db error", e));
 
 const getEvents = () => db.query("SELECT * FROM events");
 
-module.exports = { addUser, checkUser, checkEvent, addEvent, getEvents };
+const addInterest = (event, user) =>
+  db.query(`INSERT INTO interest(events_id,users_id) VALUES($1,$2)`, [
+    event,
+    user
+  ]);
+
+const removeInterest = (event, user) =>
+  db.query(`DELETE FROM interest WHERE events_id=$1 AND users_id=$2)`, [
+    event,
+    user
+  ]);
+
+module.exports = {
+  addUser,
+  checkUser,
+  checkEvent,
+  addEvent,
+  getEvents,
+  addInterest
+};
