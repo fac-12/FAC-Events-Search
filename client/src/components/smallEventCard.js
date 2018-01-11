f5d68fc0b7b457cb16e4e2efea87056f55419a27
Shannon Jensen <shannonjensen@gmail.com>
2018-01-11 18:08:20 +0000

    standardize size of event cards

--- client/src/components/smallEventCard.js
+++ client/src/components/smallEventCard.js
@@ -1,18 +1,31 @@
 import React from "react";
 
-export default props => (
-  <li key={props.data.id} className="smallEventCard_container">
-    <div className="smallEventCard_top">
-      <div className="smallEventCard_top_left">
-        <h1>{props.data.title}</h1>
-        <p>{props.data.host}</p>
+export default props => {
+  const { id, title, host, date, day, time } = props.data;
+
+  const onHover = () => {
+    console.log("hovering");
+  };
+  return (
+    <li
+      key={id}
+      onMouseEnter={title.length > 40 ? onHover : () => {}}
+      className="smallEventCard_container"
+    >
+      <div className="smallEventCard_top">
+        <div className="smallEventCard_top_left">
+          <h1>
+            {title.length > 40 ? title.slice(0, 37).concat("...") : title}
+          </h1>
+          <p>{host}</p>
+        </div>
+        <div className="smallEventCard_top_right">
+          <h2>{date}</h2>
+          <p>{day}</p>
+          <p>{time}</p>
+        </div>
       </div>
-      <div className="smallEventCard_top_right">
-        <h2>{props.data.date}</h2>
-        <p>{props.data.day}</p>
-        <p>{props.data.time}</p>
-      </div>
-    </div>
-    <p className="smallEventCard_footer">10 people interested</p>
-  </li>
-);
+      <p className="smallEventCard_footer">10 people interested</p>
+    </li>
+  );
+};
