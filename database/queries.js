868a2bfb7966b1cd65c545b3c29401d1018a2472
Shannon Jensen <shannonjensen@gmail.com>
2018-01-11 23:02:08 +0000

    Fixed serialize user error by ensuring addUser always returns full data

--- database/queries.js
+++ database/queries.js
@@ -2,18 +2,20 @@ const db = require("./db_connection");
 
 const checkUser = id => db.query("SELECT * FROM users WHERE id = $1", [id]);
 
-const addUser = profile =>
-  db.query(
-    `INSERT INTO users(id, name, github_username,email, bio) VALUES($1,$2,$3, $4, $5) RETURNING name`,
+const addUser = profile => {
+  console.log(profile);
+  return db.query(
+    `INSERT INTO users(id, name, github_username,email, bio, photo_url) VALUES($1,$2,$3,$4,$5,$6) RETURNING id, name, github_username, email, bio, photo_url`,
     [
       profile.id,
       profile.displayName,
       profile.username,
       profile._json.email,
       profile._json.bio,
-      profile.photos[0].value
+      profile._json.avatar_url
     ]
   );
+};
 
 const checkEvent = data =>
   db
@@ -24,23 +26,24 @@ const checkEvent = data =>
     .then(res => res.length > 0)
     .catch(e => console.log("db error", e));
 
-const addEvent = data => db
-  .query(
-    `INSERT INTO events(event_name, event_date, event_time, host_org_name, venue_name, venue_address, venue_postcode, event_url, event_desc) VALUES($1,$2,$3,$4,$5,$6,$7,$8, $9) RETURNING event_name`,
-    [
-      data.event_name,
-      data.event_date,
-      data.event_time,
-      data.host_org_name,
-      data.venue_name,
-      data.venue_address,
-      data.venue_postcode,
-      data.event_url,
-      data.event_desc
-    ]
-  )
-  .then(res => res[0].event_name)
-  .catch(e => console.log("db error", e));
+const addEvent = data =>
+  db
+    .query(
+      `INSERT INTO events(event_name, event_date, event_time, host_org_name, venue_name, venue_address, venue_postcode, event_url, event_desc) VALUES($1,$2,$3,$4,$5,$6,$7,$8, $9) RETURNING event_name`,
+      [
+        data.event_name,
+        data.event_date,
+        data.event_time,
+        data.host_org_name,
+        data.venue_name,
+        data.venue_address,
+        data.venue_postcode,
+        data.event_url,
+        data.event_desc
+      ]
+    )
+    .then(res => res[0].event_name)
+    .catch(e => console.log("db error", e));
 
 const getEvents = () => db.query("SELECT * FROM events");
 
