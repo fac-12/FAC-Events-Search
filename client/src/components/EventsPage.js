10ce9159c8f8ff1aff59ad12c8707660571b5409
trc5019 <trc5019@gmail.com>
2018-01-11 16:20:32 +0000

    created basic styles for eventspage

--- client/src/components/EventsPage.js
+++ client/src/components/EventsPage.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import EventCard from "./eventCard";
 import * as actions from "../actions";
 import AddEventPage from "./AddEventPage";
+import "../style.css";
 import axios from "axios";
 
 class EventsPage extends Component {
@@ -14,33 +15,35 @@ class EventsPage extends Component {
     return (
       <div>
         <AddEventPage />
-        <section>
-          <Router>
-            <ul>
-              <li>
-                <Link to="/">Add Event</Link>
-              </li>
-              <li>
-                <Link to="">My Events</Link>
-              </li>
-              <li>
-                <Link to="">Popular</Link>
-              </li>
-              <li>
-                <Link to="">Suggested</Link>
-              </li>
-              <li>
-                <Link to="">All Events</Link>
-              </li>
-            </ul>
-          </Router>
-          <input type="text" />
-          <button type="submit" />
-          <input type="date" />
-        </section>
-        <section>
-          <h1> Upcoming Events </h1>
-          <EventCard />
+        <section className="events-container">
+          <section className="sideBar">
+            <Router>
+              <ul>
+                <li>
+                  <Link to="/">Add Event</Link>
+                </li>
+                <li>
+                  <Link to="">My Events</Link>
+                </li>
+                <li>
+                  <Link to="">Popular</Link>
+                </li>
+                <li>
+                  <Link to="">Suggested</Link>
+                </li>
+                <li>
+                  <Link to="">All Events</Link>
+                </li>
+              </ul>
+            </Router>
+            <input type="text" />
+            <button type="submit" />
+            <input type="date" />
+          </section>
+          <section className="right-side">
+            <h1> Upcoming Events </h1>
+            <EventCard />
+          </section>
         </section>
       </div>
     );
--- client/src/components/eventCard.js
+++ client/src/components/eventCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import * as actions from "../actions";
 import { connect } from "react-redux";
+import "../style.css";
 
 class EventCard extends Component {
   constructor(props) {
@@ -9,34 +10,24 @@ class EventCard extends Component {
 
   render() {
     const { events } = this.props;
-    const month = [
-      "January",
-      "Febuary",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
 
     return (
-      <div>
+      <div className="event-card">
         {events.map(item => (
           <div key={item.id}>
-            <section>
-              <h2> {item.event_name} </h2>
-              <p> {item.host_org_name} </p>
-              <p> {item.venue_name} </p>
+            <section className="events-container">
+              <section className="event-Info">
+                <h2> {item.event_name} </h2>
+                <p> {item.host_org_name} </p>
+                <p> {item.venue_name} </p>
+              </section>
+              <section className="event-date">
+                <p> Jan 15 </p>
+                <p> Wed </p>
+                <p> {item.event_time} </p>
+              </section>
             </section>
-            <section>
-              <p> {console.log(item.event_date.split(":")[0])} </p>
-            </section>
-            <section>
+            <section className="info-bar">
               <button> Add Me</button>
               <button> More Info </button>
             </section>
