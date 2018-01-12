b4b8b00ddc51ccd61b34bbdc61a0e25dc95535e8
Shannon Jensen <shannonjensen@gmail.com>
2018-01-12 11:51:19 +0000

    started refactoring profile page with Redux Form

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
 import * as actions from "../actions";
 
 class ProfilePage extends Component {
@@ -9,8 +10,26 @@ class ProfilePage extends Component {
     this.state = { github_username, name, email, cohort, bio };
   }
   componentDidMount() {
-    console.log(this.props.auth);
+    console.log(this.props);
     this.props.setLocation(this.props.location.pathname);
+  }
+
+  renderInput(field) {
+    const { meta: { touched, error } } = field;
+    console.log(field);
+    return (
+      <div className={field.className}>
+        <label>{field.label}</label>
+        <input
+          className="profile_input"
+          type={field.type || "text"}
+          readOnly={field.readOnly || false}
+          placeholder={field.placeholder || ""}
+          {...field.input}
+        />
+        <div className="profile_error">{touched ? error : ""}</div>
+      </div>
+    );
   }
 
   render() {
@@ -19,66 +38,67 @@ class ProfilePage extends Component {
         <h1> Your Profile </h1>
         <div className="profile_info">
           <form className="profile_form">
-            <div className="profile_field">
-              <label htmlFor="github">Github Handle:</label>
-              <input
-                readOnly
-                id="github"
-                name="github"
-                value={`@${this.state.github_username}`}
-              />
-            </div>
-            <div className="profile_field">
-              <label htmlFor="name">Name:</label>
-              <input
-                id="name"
-                name="name"
-                value={this.state.name}
-                onChange={event => this.setState({ name: event.target.value })}
-              />
-            </div>
+            <Field
+              className="profile_field"
+              label="Github Handle"
+              name="github"
+              readOnly
+              component={this.renderInput}
+            />
+            <Field
+              className="profile_field"
+              label="Name"
+              name="name"
+              component={this.renderInput}
+            />
+            <Field
+              className="profile_field"
+              label="Email"
+              name="email"
+              component={this.renderInput}
+            />
+            <Field
+              className="profile_field"
+              label="FAC Cohort"
+              name="cohort"
+              type="number"
+              placeholder="Please enter your cohort number"
+              component={this.renderInput}
+            />
+            <Field
+              className="profile_field"
+              label="Brief Bio"
+              name="bio"
+              component={this.renderInput}
+            />
 
-            <div className="profile_field">
-              <label htmlFor="email">Email:</label>
-              <input
-                id="email"
-                name="email"
-                value={this.state.email}
-                onChange={event => this.setState({ email: event.target.value })}
-              />
-            </div>
-            <div className="profile_field">
-              <label htmlFor="cohort">FAC Cohort:</label>
-              <input
-                type="number"
-                placeholder="Please enter your cohort number"
-                id="cohort"
-                name="cohort"
-                value={this.state.cohort}
-                onChange={event =>
-                  this.setState({ cohort: event.target.value })
-                }
-              />
-            </div>
-            <div className="profile_field">
-              <label htmlFor="bio">Brief Bio:</label>
-              <textarea
-                id="bio"
-                name="bio"
-                onChange={event => this.setState({ bio: event.target.value })}
-              >
-                {this.state.bio}
-              </textarea>
-            </div>
             <button type="submit">Submit</button>
           </form>
-          <img src={this.props.auth[0].photo_url} />
+          <img src={this.props.auth[0].photo_url} alt="user" />
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ auth }) => ({ auth });
+function validate(values) {
+  const errors = {};
+  if (!values.name) {
+    errors.name = "Please enter a name";
+  } else if (values.name.length > 100) {
+    errors.name = "Names cannot exceed 100 characters";
+  }
+  if (!values.email) {
+    errors.email = "Please enter your email";
+  }
+  if (!values.cohort) {
+    errors.cohort = "Please enter your cohort number";
+  }
+  return errors;
+}
 
-export default connect(mapStateToProps, actions)(ProfilePage);
+const mapStateToProps = ({ auth }) => ({ auth, initialValues: auth[0] });
+
+export default reduxForm({ validate, form: "ProfileForm" })(
+  connect(mapStateToProps, actions)(ProfilePage)
+);
