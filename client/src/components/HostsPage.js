a66cfe5aba42336009c791bd3bd7b7d11cdc38fd
Shannon Jensen <shannonjensen@gmail.com>
2018-01-12 16:16:07 +0000

    action for adding host done, reducer needs to be finished

--- client/src/components/HostsPage.js
+++ client/src/components/HostsPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
-import axios from "axios";
+import HostsAddForm from "./HostsAddForm";
 
 class HostsPage extends Component {
   constructor() {
@@ -12,6 +12,17 @@ class HostsPage extends Component {
   componentDidMount() {
     this.props.setLocation(this.props.location.pathname);
   }
+
+  checkMeetup = url => {
+    if (url.includes("www.meetup.com")) {
+      this.props.addHost(url);
+    }
+  };
+
+  clickHandler = e => {
+    e.preventDefault();
+    this.checkMeetup(this.state.url);
+  };
 
   render() {
     return (
@@ -47,39 +58,7 @@ class HostsPage extends Component {
             </form>
           </article>
         </section>
-        <section className="hosts_options">
-          <h2>I am interested in events organized by:</h2>
-          <label>
-            <input type="checkbox" /> Node Girls
-          </label>
-          <label>
-            <input type="checkbox" /> Queer Code London
-          </label>
-          <label>
-            <input type="checkbox" /> Founders & Coders
-          </label>
-          <label>
-            <input type="checkbox" /> The JS Roundabout
-          </label>
-          <label>
-            <input type="checkbox" /> UX Playground
-          </label>
-          <label>
-            <input type="checkbox" /> London Halfstack
-          </label>
-          <label>
-            <input type="checkbox" /> Ladies of Code
-          </label>
-          <label>
-            <input type="checkbox" /> Codebar
-          </label>
-          <label>
-            <input type="checkbox" /> Journocoders
-          </label>
-          <label>
-            <input type="checkbox" /> London Node Users Group
-          </label>
-        </section>
+        <HostsAddForm />
       </div>
     );
   }
