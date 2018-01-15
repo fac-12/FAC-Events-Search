30a676cb22880d5263bf01415756d494aa158349
Shannon Jensen <shannonjensen@gmail.com>
2018-01-15 14:27:32 +0000

    remove commented out code

--- client/src/components/HostsAddForm.js
+++ client/src/components/HostsAddForm.js
@@ -3,22 +3,6 @@ import { connect } from "react-redux";
 import { Field, reduxForm, initialize } from "redux-form";
 import * as actions from "../actions";
 
-// renderField(field) {
-//   const { meta: { touched, error } } = field;
-//   // const className = `form-container ${touched && error ? "error" : ""}`;
-//   return (
-//     <div className="form_field">
-//       <label>{field.label}</label>
-//       <input type="checkbox" {...field.input} />
-//       <div>{touched ? error : ""}</div>
-//     </div>
-//   );
-// }
-
-// onSubmit(values) {
-
-// }
-
 class HostsAddForm extends Component {
   constructor(props) {
     super(props);
@@ -61,57 +45,6 @@ class HostsAddForm extends Component {
       <form className="hosts_options">
         <h2>I am interested in events organized by:</h2>
         <ul>{this.renderFields(this.props.initialValues)}</ul>
-
-        {/* <Field
-          label="node-girls"
-          name="host_org_name"
-          component={this.renderField}
-        />
-        <Field
-          label="Queer-Code-London"
-          name="host_org_name"
-          component={this.renderField}
-        />
-        <Field
-          label="Founders-&-Coders"
-          name="host_org_name"
-          component={this.renderField}
-        />
-        <Field
-          label="The-JS-oundabout"
-          name="host_org_name"
-          component={this.renderField}
-        />
-        <Field
-          label="UX-Playground"
-          name="venue_address"
-          component={this.renderField}
-        />
-        <Field
-          label="London-Halfstack"
-          name="venue_address"
-          component={this.renderField}
-        />
-        <Field
-          label="Ladies-of-Code"
-          name="venue_address"
-          component={this.renderField}
-        />
-        <Field
-          label="Codebar"
-          name="venue_address"
-          component={this.renderField}
-        />
-        <Field
-          label="Journocoders"
-          name="venue_address"
-          component={this.renderField}
-        />
-        <Field
-          label="London Node User Group"
-          name="venue_address"
-          component={this.renderField}
-        /> */}
       </form>
     );
   }
