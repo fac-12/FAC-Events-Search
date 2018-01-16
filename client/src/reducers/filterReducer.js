dbb7c00cf9ca4a51d745b244be43fec8d11f60a4
Shannon Jensen <shannonjensen@gmail.com>
2018-01-16 23:48:06 +0000

    rolling back changes

--- client/src/reducers/filterReducer.js
+++ client/src/reducers/filterReducer.js
@@ -2,13 +2,15 @@ import { SELECT_FILTER } from "../actions/types";
 
 const defaultState = {
   filter: "all",
-  filteredIds: []
+  startDate: null,
+  endDate: null,
+  searchTerm: null
 };
 
 export default function(state = defaultState, action) {
   switch (action.type) {
   case SELECT_FILTER:
-    return state;
+    return action.payload;
   default:
     return state;
   }
